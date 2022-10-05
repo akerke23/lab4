8f28fcebe8c50137d4f14cbf357f7f717bdf7f1f
akerke23 <myrzalyakerke02@gmail.com>
2022-10-05 14:19:52 +0600

    first commit

---
+++ lab4.py
@@ -0,0 +1,52 @@
+"""
+#1 есеп
+s = 'abcdefg'
+print(s[1])
+print(s[-1])
+print(s[1:3])
+print(s[1:-1])
+print(s[:3])
+print(s[2:])
+print(s[:-1])
+print(s[::2])
+print(s[1::2])
+print(s[::-1])
+
+#2 есеп ауыстыру
+print('Hello'.replace('l', 'L'))
+
+#3 есеп а әріпінің суммасы
+print('Abracadabra'.count('a'))
+
+#4 есеп іздеу
+S = 'Hello'
+print(S.find('l'))
+
+#5 есеп үлкен әріпке ауыстыру
+a = "Hello, World!"
+print(a.upper())
+
+#6 есеп кішкентай әріпке ауыстыру
+a = "Hello, World!"
+print(a.lower())
+
+#7 есеп сөзді центрге қойып қалған бос орынды жұлдызшамен толтырады
+txt = "banana"
+x = txt.center(20, '*')
+print(x)
+
+#8 есеп каждый элементті жеке жеке қарастыру
+txt = "welcome to the jungle"
+x = txt.split()
+print(x)
+
+#9 есеп әріптерді кері жақа ауыстыру
+txt = "Hello My Name Is PETER"
+x = txt.swapcase()
+print(x)
+
+#10 есеп  сөзді мәтінге қосу
+txt = "     banana     "
+x = txt.strip()
+print("of all fruits", x, "is my favorite")
+"""
